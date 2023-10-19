8f1e1c091f3ea3d67a23b701a9b8f7ff9341bdd3
Luis Miguel <luismiguelalvarezmarin109@gmail.com>
2023-10-19 16:07:23 -0500

    feat: :sparkles: create user , get user finish

--- notes_app_sps/server/app.js
+++ notes_app_sps/server/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import notesRoute from './routes/notes_route.js'
-
+import userRoute from './routes/user_note.js'
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api",notesRoute)
+app.use("/api",userRoute)
 
 export default app;
